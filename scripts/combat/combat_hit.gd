f567e58a4ae5fd462b53dd748ff09ac6c4e07f19
drok123 <52803485+drok123@users.noreply.github.com>
2026-09-05 11:37:22 -0500

    Extend combat hit payload for weapon rarity and gore

--- scripts/combat/combat_hit.gd
+++ scripts/combat/combat_hit.gd
@@ -4,6 +4,7 @@ extends RefCounted
 var source: Node3D = null
 var ability_id: StringName = &""
 var weapon_id: StringName = &"unarmed"
+var weapon_rarity: StringName = &"common"
 var damage_type: StringName = &"blunt"
 var hit_location: StringName = &"body"
 
@@ -11,3 +12,4 @@ var damage: float = 0.0
 var impulse: Vector3 = Vector3.ZERO
 var wall_stun_window: float = 0.0
 var hitstop: float = 0.0
+var gore_power: float = 0.0
